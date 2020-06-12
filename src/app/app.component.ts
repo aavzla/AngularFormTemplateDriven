473ef278ceafc33b33c7f9ac16abb4e31be5ff29
AAvilan <andres.avilan@nurun.com>
2020-06-12 12:32:22 -0400

    TD: Submitting and Using the Form - Setup the form with a local reference to let know Angular that we want to have access to the Form JS Object created by Angular and we use ngSubmit to map the submit of the form to the event and passing the local reference as parameter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   suggestUserName() {
     const suggestedName = 'Superuser';
+  }
+
+  onSubmit(
+    //The HTMLFormELement is the native element of a local reference.
+    //form: HTMLFormElement
+    form: NgForm
+  ) {
+    console.log(this.constructor.name + " - Submitted!");
+    console.log(this.constructor.name + ' - This is the form', form);
+    console.log(this.constructor.name + ' - Here are the JS object of form.value', form.value);
+    console.log(this.constructor.name + ' - Form username value is ' + form.value.username);
+    console.log(this.constructor.name + ' - Form email value is ' + form.value.email);
+    console.log(this.constructor.name + ' - Form secret value is ' + form.value.secret);
   }
 }
